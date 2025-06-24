e2022b37d5e821111282a0e2ac251a4275267d43
albertoastolfo <alberto.asto@gmail.com>
2025-06-24 16:08:07 +0100

    replaced value 2 with value

--- fractal.py
+++ fractal.py
@@ -1,20 +1,21 @@
 from matplotlib.pyplot import *
 from numpy import *
-max=2
-min=-2
-c=2
+value = 2
+max=value
+min=-value
+c=value
 image_size = 1240
 A=zeros((image_size, image_size))
 for px in range(image_size):
- x0=(px/image_size)*(max-min)-2
+ x0=(px/image_size)*(max-min)-value
  for py in range(image_size):
-  y0=(py/image_size)*(max-min)-2
+  y0=(py/image_size)*(max-min)-value
   x=0.0
   y=0.0
   it=0
   while (((x*x)+(y*y))<(c*c)) and it<256:
    xt=(x*x)-(y*y)+x0
-   y=2.0*x*y+y0
+   y=value*x*y+y0
    x=xt
    it=it+1
   A[px,py]=it
